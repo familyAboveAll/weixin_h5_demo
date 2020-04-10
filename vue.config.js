10664085c1bdef4bcb87cae7f8096d8883f7d73e
ymy <>
2020-04-10 13:31:07 +0800

    配置px2rem

--- vue.config.js
+++ vue.config.js
@@ -27,11 +27,21 @@ module.exports = {
   },
   // css相关配置
   css: {
+    loaderOptions: {
+      css: {},
+      postcss: {
+        plugins: [
+          require('postcss-px2rem')({
+            remUnit: 37.5
+          })
+        ]
+      }
+    },
     // 是否使用css分离插件 ExtractTextPlugin
     extract: true,
     // 开启 CSS source maps?
     sourceMap: false,
     // 启用 CSS modules for all css / pre-processor files.
-    modules: false
+    requireModuleExtension: false
   },
 };
